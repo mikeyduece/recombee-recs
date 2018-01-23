383710a436e3491502831514e0b3562b4a4d0d19
Mike Heft <mikeheft@gmail.com>
2018-01-23 13:59:48 -0700

    Add Routes to allow for link clicks"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movies: [],
+      recs: [],
+    }
+  }
+
+  componentDidMount(){
+    this.getAllMovies()
+  }
+
+  getAllMovies(){
+    fetch('https://recombee-api.herokuapp.com/api/v1/all_movies')
+      .then(response => response.json())
+      .then(data => {
+        debugger
+        this.setState({movies: data})
+      })
+  }
+
+  handleMovies(){
+    let movies = this.state.movies
+    // debugger
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.handleMovies()}
       </div>
     );
   }
